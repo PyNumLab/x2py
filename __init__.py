e9d44c2e3e59ecb2caf247ad787f4e70ff2ea1d1
Said Hadjout <said-ctb@live.fr>
2026-05-04 19:23:57 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,8 +1,5 @@
 # -*- coding: utf-8 -*-
-#------------------------------------------------------------------------------------------#
-# This file is part of Pyccel which is released under MIT License. See the LICENSE file or #
-# go to https://github.com/pyccel/pyccel/blob/devel/LICENSE for full license details.      #
-#------------------------------------------------------------------------------------------#
+
 """Compatibility layer for the minimal Fortran signature parser."""
 
 from fortran_parser import (
