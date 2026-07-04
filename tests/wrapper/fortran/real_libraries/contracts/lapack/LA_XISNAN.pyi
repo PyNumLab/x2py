c6f79e26584c27e3f4065d76ba366b9b2326bd4c
said <said-ctb@live.fr>
2026-07-04 15:17:13 +0100

    fix lapack fixture errors

--- tests/wrapper/fortran/real_libraries/contracts/lapack/LA_XISNAN.pyi
+++ tests/wrapper/fortran/real_libraries/contracts/lapack/LA_XISNAN.pyi
@@ -1,19 +1,23 @@
 @bind("SISNAN")
+@native_call([Addr(Arg(0))])
 def sisnan(
-    x: Addr(Float32)
+    x: Float32
 ) -> Bool: ...
 
 @bind("DISNAN")
+@native_call([Addr(Arg(0))])
 def disnan(
-    x: Addr(Float64)
+    x: Float64
 ) -> Bool: ...
 
 @overload("SISNAN")
+@native_call([Addr(Arg(0))])
 def la_isnan(
-    x: Addr(Float32)
+    x: Float32
 ) -> Bool: ...
 
 @overload("DISNAN")
+@native_call([Addr(Arg(0))])
 def la_isnan(
-    x: Addr(Float64)
+    x: Float64
 ) -> Bool: ...
