1ae75f9884574b7a143ec3d464758849d07b7cfc
said <said-ctb@live.fr>
2026-07-10 23:10:47 +0100

    update fmodule_vars_f90.pyi

--- tests/wrapper/fortran/build_from_pyi/contracts/module_variables/fmodule_vars_f90.pyi
+++ tests/wrapper/fortran/build_from_pyi/contracts/module_variables/fmodule_vars_f90.pyi
@@ -1,6 +1,21 @@
 from x2py.contracts import Final, Float64, Int32
 
+class rgb_color:
+    def __init__(
+        self,
+        *,
+        r: Int32 = ...,
+        g: Int32 = ...,
+        b: Int32 = ...
+    ) -> None: ...
+
+    r: Int32
+    g: Int32
+    b: Int32
+
 nmax: Final[Int32] = 12
+
+black: Final[rgb_color]
 
 counter: Int32
 
@@ -13,3 +28,5 @@ def summarize() -> Int32: ...
 def scaled_counter() -> Float64: ...
 
 def next_local() -> Int32: ...
+
+def black_sum() -> Int32: ...
